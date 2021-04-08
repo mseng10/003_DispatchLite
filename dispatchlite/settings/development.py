5033cc78b3c1dccc6fc711825981d225842a9767
Mathew Seng <mathewseng@gmail.com>
2021-04-08 18:06:59 -0500

    Import settings from source repo
    This removes all email functionality and static content as we do not it for just a restful API.

---
+++ dispatchlite/settings/development.py
@@ -0,0 +1,20 @@
+from PGSCM.settings.common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'j04#7k2y*#1y%)54it3h1ve^2#iafyq-%^d$6sw1ceg58j-m!h'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'temppassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+ALLOWED_HOSTS = ['*']
---
+++ dispatchlite/settings/production.py
@@ -0,0 +1,28 @@
+from PGSCM.settings.common import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = [""]
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
+
+hostname = os.environ['DBHOST']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ['DBUSER'] + "@" + hostname,
+        'PASSWORD': os.environ['DBPASS']
+    }
+}
