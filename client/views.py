6b8f8de734ee46f86ef7db3174ba3d67fd6e09e2
Mathew Seng <mathewseng@gmail.com>
2021-04-08 20:33:13 -0500

    Create get request for when a user wishes to get their information
    This will not return the api key.

--- client/views.py
+++ client/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from client.models import Client
+from client.serializers import ClientSerializer
 
 # Create your views here.
+@csrf_exempt
+def client(request):
+    if request.method == 'GET':
+        snippets = Client.objects.get(key='41AZXWJ4aIfKPOlTFsVs')
+        serializer = ClientSerializer(snippets)
+        return JsonResponse(serializer.data, safe=False)
