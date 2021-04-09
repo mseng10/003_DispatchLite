2789af3b56842f08c971431e84fbff99a6327103
Mathew Seng <mathewseng@gmail.com>
2021-04-08 23:12:32 -0500

    Allow admins to create Templates in the database
    This will allow use to create templates in the database (dummy data).

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Template
 
 class ClientAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Client, ClientAdmin)
+
+class TemplatesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Template, TemplatesAdmin)
