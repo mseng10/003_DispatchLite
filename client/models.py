7db962097bc4b4de462af766b1d4f8d40fb7f259
Mathew Seng <mathewseng@gmail.com>
2021-04-08 23:11:42 -0500

    Create Template model
    This will mimic the Dispatch template object. I have added a url for an easier time returning the data we need.

--- client/models.py
+++ client/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import random
 from django.utils.crypto import get_random_string
 import os
+from django.contrib.postgres.fields import ArrayField
 
 if 'WEBSITE_HOSTNAME' in os.environ:
     url = 'finalurl'
@@ -24,3 +25,26 @@ class Client(models.Model):
         self.key = get_random_string(20)
         print(self.key)
         super(Client, self).save(*args, **kwargs)
+
+class Template(models.Model):
+    class Types(models.TextChoices):
+        EMAIL = 'EMAIL'
+        LETTER = 'LETTER'
+        TWIML = 'TWIML'
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
+    dynamic = models.BooleanField(blank=True, null=True)
+    tags = ArrayField(models.CharField(max_length=55),blank=True,null=True)
+    content = models.CharField(max_length=100, blank=True)
+    comments = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=7, choices=Types.choices)
+    client = models.CharField(max_length=100, default=url+'client', editable=False)
+    url = models.CharField(max_length=100, default=url+'templates/', editable=False) ## not Dispatch just helper
+
+    def save(self, *args, **kwargs):
+        identifier = random.randint(100000000, 999999999)
+        self.id = identifier
+        self.url = self.url + str(identifier)
+        super(Template, self).save(*args, **kwargs)
