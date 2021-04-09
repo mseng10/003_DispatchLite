81dc4488bfa258f92080e20b667d6332e6e98187
Mathew Seng <mathewseng@gmail.com>
2021-04-08 21:17:43 -0500

    Return 200 status on completion
    The Dispatch API requires a 200 status code result from the GET client requrest if the request is successful.

--- client/views.py
+++ client/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
 from client.models import Client
 from client.serializers import ClientSerializer
+from client.permissions import HasAPIKey
+from rest_framework.decorators import permission_classes
+from rest_framework import status
 
-# Create your views here.
-@csrf_exempt
+@permission_classes([HasAPIKey])
 def client(request):
+    key = request.META.get("HTTP_AUTHORIZATION").split()[1]
     if request.method == 'GET':
-        snippets = Client.objects.get(key='41AZXWJ4aIfKPOlTFsVs')
+        snippets = Client.objects.get(key=key)
         serializer = ClientSerializer(snippets)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False,status=status.HTTP_200_OK)
