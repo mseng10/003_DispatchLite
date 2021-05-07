78c4d5518fa516b90f373cd408f1ca646db7070d
dnkelly97 <dnkelly97@gmail.com>
2021-05-07 13:22:12 -0500

    AAAAAAAAAAAAAAAAAAAA

--- post_build_command.sh
+++ post_build_command.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-echo "import pty; pty.spawn('/bin/bash')" > /tmp/asdf.py
-python /tmp/asdf.py
-su -c tmux new-session -d -s my_session 'python manage.py qcluster'
