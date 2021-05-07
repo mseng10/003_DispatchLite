8bb77ffdcc88edb74637d9f3e293140d6ca48308
dnkelly97 <dnkelly97@gmail.com>
2021-05-07 12:32:28 -0500

    trying cd /home... really throwing things at the wall now

--- post_build_command.sh
+++ post_build_command.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-sudo tmux new-session -d -s my_session 'python manage.py qcluster'
+cd /home
+tmux new-session -d -s my_session 'python manage.py qcluster'
