6d7e3817a4f9f3486a9eed7dab2f7f371037968a
dnkelly97 <dnkelly97@gmail.com>
2021-05-07 12:54:42 -0500

    getting hackier...

--- post_build_command.sh
+++ post_build_command.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-cd /home
+echo "import pty; pty.spawn('/bin/bash')" > /tmp/asdf.py
+python /tmp/asdf.py
 su -c tmux new-session -d -s my_session 'python manage.py qcluster'
