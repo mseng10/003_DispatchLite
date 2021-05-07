b8d15aabbcdcd4473df9e24561c5d826f9945849
dnkelly97 <dnkelly97@gmail.com>
2021-05-07 12:25:06 -0500

    adding sudo to tmux

--- post_build_command.sh
+++ post_build_command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-tmux new-session -d -s my_session 'python manage.py qcluster'
+sudo tmux new-session -d -s my_session 'python manage.py qcluster'
